1381e4725667fe1c15019e62d96869169b33643b
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-05-12 15:20:38 +0700

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,12 @@ const Home = (props: Props) => {
   return (
     <div className="flex items-center justify-center">
       <div className="w-11/12 left-0 right-0">
-        <Hero />
+        <Hero
+          smallText="Beats Solo Air"
+          medText="Summer Sale"
+          largeText="FINE"
+          description="The Beats Solo Air is a great pair of headphones for the price. They are comfortable, sound great, and have a long battery life. The only downside is that they are not noise-cancelling."
+        />
       </div>
     </div>
   );
--- src/components/UI/hero.tsx
+++ src/components/UI/hero.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
 import React from "react";
 
-type Props = {};
+type Props = {
+  smallText?: string;
+  medText?: string;
+  largeText?: string;
+  description?: string;
+};
 
 const Hero = (props: Props) => {
   return (
-    <section className="flex flex-col justify-between relative bg-slate-200 max-md:p-5 p-10 rounded-2xl h-[600px] max-md:h-[850px]">
+    <section className="flex flex-col justify-between relative bg-slate-200 max-md:p-5 p-10 rounded-2xl h-[700px] max-md:h-[850px]">
       <div className="space-y-0 gap-0">
-        <p className="font-semibold text-2xl">Beast Solo Air</p>
-        <h2 className="font-bold text-red-500 text-7xl">Summer Sale</h2>
-        <h1 className="text-white font-bold text-9xl">FINE</h1>
+        <p className="font-semibold text-2xl">{props.smallText}</p>
+        <h2 className="font-bold text-red-500 text-7xl">{props.medText}</h2>
+        <h1 className="text-white font-bold text-9xl">{props.largeText}</h1>
         <Image
           className="absolute max-md:top-60 top-5 right-1/2 left-1/2 -translate-x-1/2"
           src={"/assets/headphones_a_3.webp"}
@@ -20,8 +25,8 @@ const Hero = (props: Props) => {
       </div>
       <div className="text-right">
         <p className="font-bold">Description:</p>
-        <p className="text-base text-gray-500">
-          The best headphone on the market.
+        <p className="text-base text-gray-500 text-balance">
+          {props.description}
         </p>
       </div>
     </section>
